2166e997a5eeda7382f24f20a99dc0dd69115e99
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-07-25 02:55:25 +0530

    footer working

--- common/footer.php
+++ common/footer.php
@@ -34,5 +34,90 @@
         </div>
     </div>
     <hr>
+    <div class="footer d-flex">
+        <div class="footer-item logo"></div>
+        <div class="footer-item address">
+        <div class="heading-footer h5">
+            <b>
+              Address
+            </b>
+          </div>
+          <div class="content-footer d-flex flex-column">
+            <span>
+              203. Alliance Tower 2
+            </span> 
+            <span>
+              LSC 7, Savita Vihar
+            </span>
+            <span>
+              Delhi, 110092
+            </span>
+            <span>
+              India</div>
+            </span>
+            </div>
+        </div>
+        <div class="footer-item explore">
+        <div class="heading-footer h5 ">
+              <b>
+                Explore
+              </b>
+            </div>
+            <div class="content-footer d-flex flex-column">
+              <span>
+                Blog
+              </span>
+              <span>
+                Become a partner
+              </span>
+              <span>
+                Coupons
+              </span>
+            </div>
+        </div>
+        <div class="footer-item help">
+        <div class="heading-footer h5">
+              <b>
+                Help
+              </b>
+              
+            </div>
+            <div class="content-footer d-flex flex-column">
+              <span>
+                Contact us
+              </span>
+              <span>
+                Privacy policies
+              </span>
+              <span>
+                Terms and conditions
+              </span>
+            </div>
+        </div>
+        <div class="footer-item subscribe">
+        <div class="heading-footer h5">
+              <b>
+                Subscribe
+              </b>
+            </div>
+            <div class="content-footer content-footer-submit d-flex flex-column">
+              <span>
+                Sign up for News Letter
+              </span>
+              <span>
+                <form action="subscribe.html">
+                  <input class="email-input" type="text" name="email" placeholder="Your email address">
+                  <input class="submit-footer" type="submit" name="submit" value="Submit">
+                </form>
+              </span>
+              <span>
+                Register now to get updates on
+              </span>
+              <span>
+                promotions and coupons
+              </span>
+            </div>
+        </div>
+    </div>
     <hr>
 </div>

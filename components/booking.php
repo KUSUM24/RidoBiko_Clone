e4a45bcdd98c64feb0f6062922c2ba3aea7781f5
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-08-04 01:00:02 +0530

    booking responsive

--- components/booking.php
+++ components/booking.php
@@ -4,18 +4,22 @@
             Why Book with Ridobiko ?
         </div>
     <hr>
-    <div class="row images-main">
-        <div class="img-col col-md-3">
-            <img class="book-1 img-book" src="images/booking/book-1.png" alt="error">
+    <div class="row images-main justify-content-around ">
+        <div class="pair-images d-flex justify-content-between ">
+            <div class="img-col col-md-3">
+                <img class="book-1 img-book" src="images/booking/book-1.png" alt="error">
+            </div>
+            <div class="img-col col-md-3">
+                <img class="book-2 img-book " src="images/booking/book-2.png" alt="error">
+            </div>
         </div>
-        <div class="img-col col-md-3">
-            <img class="book-2 img-book " src="images/booking/book-2.png" alt="error">
-        </div>
-        <div class="img-col col-md-3">
-            <img class="book-3 img-book" src="images/booking/book-3.png" alt="error">
-        </div>
-        <div class="img-col col-md-3">
+        <div class="pair-images d-flex justify-content-between ">
+            <div class="img-col col-md-3">
+                <img class="book-3 img-book" src="images/booking/book-3.png" alt="error">
+            </div>
+            <div class="img-col col-md-3">
             <img class="book-4 img-book" src="images/booking/book-4.png" alt="error">
+            </div>
         </div>
     </div>
 </div>

24f13b60de5151da66797c6af3f68f20262633a0
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-08-23 12:28:48 +0530

    right body changed

--- dashboard-page.php
+++ dashboard-page.php
@@ -50,117 +50,55 @@
       <li class="item">Security Deposit</li>
     </div>
   </div>
-  <div class="right-body card">
-    <div class="heading">My ACCOUNT</div>
-    <div class="card-body">
-      <div class="account-form card">
-        <div class="account-form--heading">Account Details</div>
-        <div class="row form-group px-3 align-items-center">
-          <div class="col-sm-2 p-t5">Name</div>
-          <div class="col-sm-4">
-            <input
+  <!-- <div class="d-flex flex-column"> -->
+    <div class="right-body card w-100">
+      <div class="heading">My ACCOUNT</div>
+      <div class="card-body">
+        <div class="account-form card">
+          <div class="account-form--heading">Account Details</div>
+          <div class="row form-group px-3 align-items-center">
+            <div class="col-sm-2 p-t5">Name</div>
+            <div class="col-sm-4">
+              <input
               class="form-control size-13"
               name="name"
               placeholder="John Doe"
               type="text"
-            />
+              />
+            </div>
           </div>
-        </div>
-        <div class="row form-group px-3 align-items-center">
-          <div class="col-sm-2 p-t5">Email</div>
-          <div class="col-sm-4">
-            <input
+          <div class="row form-group px-3 align-items-center">
+            <div class="col-sm-2 p-t5">Email</div>
+            <div class="col-sm-4">
+              <input
               class="form-control size-13"
               name="email"
               placeholder="johndoe@gmail.com"
               type="email"
-            />
+              />
+            </div>
           </div>
-        </div>
-        <div class="row form-group px-3 align-items-center">
-          <div class="col-sm-2 p-t5">Mobile</div>
-          <div class="col-sm-4">
-            <input
+          <div class="row form-group px-3 align-items-center">
+            <div class="col-sm-2 p-t5">Mobile</div>
+            <div class="col-sm-4">
+              <input
               class="form-control size-13"
               name="mobile"
               placeholder="10 digit mobile no."
               type="number"
-            />
-          </div>
-        </div>
-        <div class="account-form--heading">Current Address</div>
-        <div class="d-flex">
-          <div class="row form-group px-3 align-items-center w-100">
-            <div class="col-sm-4 p-t5">Address 1</div>
-            <div class="col-8">
-              <input
-                class="form-control size-13"
-                name="address-1"
-                placeholder="Apartment, House number"
-                type="text"
               />
             </div>
           </div>
-          <div class="row form-group px-3 align-items-center w-100">
-            <div class="col-sm-4 p-t5">Address 2</div>
-            <div class="col-8">
-              <input
-                class="form-control size-13"
-                name="address-2"
-                placeholder="Landmark, Street Name"
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="d-flex">
-          <div class="row form-group px-3 align-items-center w-100">
-            <div class="col-sm-4 p-t5">City</div>
-            <div class="col-8">
-              <input
-                class="form-control size-13"
-                name="city"
-                placeholder="Mumbai, New York"
-                type="text"
-              />
-            </div>
-          </div>
-          <div class="row form-group px-3 align-items-center w-100">
-            <div class="col-sm-4 p-t5">Zipcode</div>
-            <div class="col-8">
-              <input
-                class="form-control size-13"
-                name="zipcode"
-                placeholder="500000"
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="permanent-address form-check">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            value="Permanent"
-            id="permanentAddress"
-            onclick="permanentAddress()"
-            checked
-          />
-          <label class="form-check-label" for="permanentAddress">
-            Permanent Address same as current Address
-          </label>
-        </div>
-        <div id="permanentAddressForm" style="display: none">
-          <div class="account-form--heading">Permanent Address</div>
+          <div class="account-form--heading">Current Address</div>
           <div class="d-flex">
             <div class="row form-group px-3 align-items-center w-100">
               <div class="col-sm-4 p-t5">Address 1</div>
               <div class="col-8">
                 <input
-                  class="form-control size-13"
-                  name="address-1"
-                  placeholder="Apartment, House number"
-                  type="text"
+                class="form-control size-13"
+                name="address-1"
+                placeholder="Apartment, House number"
+                type="text"
                 />
               </div>
             </div>
@@ -168,11 +106,11 @@
               <div class="col-sm-4 p-t5">Address 2</div>
               <div class="col-8">
                 <input
-                  class="form-control size-13"
+                class="form-control size-13"
                   name="address-2"
-                  placeholder="landmark, Street Name"
+                  placeholder="Landmark, Street Name"
                   type="text"
-                />
+                  />
               </div>
             </div>
           </div>
@@ -181,10 +119,10 @@
               <div class="col-sm-4 p-t5">City</div>
               <div class="col-8">
                 <input
-                  class="form-control size-13"
-                  name="city"
-                  placeholder="Mumbai, New York"
-                  type="text"
+                class="form-control size-13"
+                name="city"
+                placeholder="Mumbai, New York"
+                type="text"
                 />
               </div>
             </div>
@@ -192,21 +130,85 @@
               <div class="col-sm-4 p-t5">Zipcode</div>
               <div class="col-8">
                 <input
-                  class="form-control size-13"
-                  name="zipcode"
-                  placeholder="500000"
+                class="form-control size-13"
+                name="zipcode"
+                placeholder="500000"
                   type="text"
-                />
+                  />
               </div>
             </div>
           </div>
+          <div class="permanent-address form-check">
+            <input
+            class="form-check-input"
+            type="checkbox"
+            value="Permanent"
+            id="permanentAddress"
+            onclick="permanentAddress()"
+            checked
+            />
+            <label class="form-check-label" for="permanentAddress">
+              Permanent Address same as current Address
+            </label>
+          </div>
+          <div id="permanentAddressForm" style="display: none">
+            <div class="account-form--heading">Permanent Address</div>
+            <div class="d-flex">
+              <div class="row form-group px-3 align-items-center w-100">
+                <div class="col-sm-4 p-t5">Address 1</div>
+                <div class="col-8">
+                  <input
+                  class="form-control size-13"
+                  name="address-1"
+                  placeholder="Apartment, House number"
+                  type="text"
+                  />
+                </div>
+              </div>
+              <div class="row form-group px-3 align-items-center w-100">
+                <div class="col-sm-4 p-t5">Address 2</div>
+                <div class="col-8">
+                  <input
+                  class="form-control size-13"
+                    name="address-2"
+                    placeholder="landmark, Street Name"
+                    type="text"
+                    />
+                </div>
+              </div>
+            </div>
+            <div class="d-flex">
+              <div class="row form-group px-3 align-items-center w-100">
+                <div class="col-sm-4 p-t5">City</div>
+                <div class="col-8">
+                  <input
+                    class="form-control size-13"
+                    name="city"
+                    placeholder="Mumbai, New York"
+                    type="text"
+                    />
+                  </div>
+              </div>
+              <div class="row form-group px-3 align-items-center w-100">
+                <div class="col-sm-4 p-t5">Zipcode</div>
+                <div class="col-8">
+                  <input
+                  class="form-control size-13"
+                    name="zipcode"
+                    placeholder="500000"
+                    type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="submit-dashboard">
+          <button type="submit" class="submit-btn">UPDATE </button>
         </div>
       </div>
-      <div class="submit-dashboard">
-        <button type="submit" class="submit-btn">UPDATE </button>
-      </div>
     </div>
-  </div>
+  <!-- </div> -->
 </div>
 <script>
   const permanentAddress = () => {

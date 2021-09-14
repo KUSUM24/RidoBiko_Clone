6728673e32613754cddfaafe039de056f804bf24
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-14 13:21:22 +0530

    change functionality added

--- dashboard-page.php
+++ dashboard-page.php
@@ -1491,10 +1491,10 @@
               accept=".png,.jpg,.jpeg"
               placeholder="John Doe"
               type="file"
-              onChange="img_pathUrl(this,'front-img','btn-preview-section');"
+              onChange="img_pathUrl(this,'front-img','btn-preview-section-0');"
               />
               <img src="" class="preview-img mt-2" style="display:none" id="front-img" alt="Front-image">
-              <div id="btn-preview-section" class="profile-view-image align-items-between" style="display:none" >
+              <div id="btn-preview-section-0" class="profile-view-image align-items-between" style="display:none" >
                   <button class="btn btn-primary m-2 ml-3">Save</button>
                   <label for="aadhar-front" class="label-preview--change btn btn-danger m-2">Change</label>
               </div>
@@ -1502,46 +1502,58 @@
           </div>
           <div class="row form-group px-3 align-items-center">
             <label class="col-sm-2 p-t5 m-0 pr-0" for="aadhar-back">Back Side</label>
-            <div class="col-sm-6 p-0 d-flex flex-row align-items-center">
+            <div class="col-sm-10 p-0 d-flex flex-row align-items-center">
               <input
               name="aadhar-back"
               id="aadhar-back"
               accept=".png,.jpg,.jpeg"
               placeholder="John Doe"
               type="file"
-              onChange="img_pathUrl(this,'back-img');"
+              onChange="img_pathUrl(this,'back-img','btn-preview-section-1');"
               />
               <img src="" class="preview-img mt-2" style="display:none" id="back-img" alt="Back-image">
+              <div id="btn-preview-section-1" class="profile-view-image align-items-between" style="display:none" >
+                  <button class="btn btn-primary m-2 ml-3">Save</button>
+                  <label for="aadhar-back" class="label-preview--change btn btn-danger m-2">Change</label>
+              </div>
             </div>
           </div>
           <div class="sub-heading">Pan Card</div>
           <div class="row form-group px-3 align-items-center">
             <label class="col-sm-2 p-t5 m-0" for="pancard">Pan Card</label>
-            <div class="col-sm-6 p-0 d-flex flex-row align-items-center">
+            <div class="col-sm-10 p-0 d-flex flex-row align-items-center">
               <input
               name="pancard"
               id="pancard"
               accept=".png,.jpg,.jpeg"
               placeholder="Pan Card"
               type="file"
-              onChange="img_pathUrl(this,'pan-img');"
+              onChange="img_pathUrl(this,'pan-img','btn-preview-section-2');"
               />
               <img src="" class="preview-img mt-2" style="display:none" id="pan-img" alt="Pan-image">
+              <div id="btn-preview-section-2" class="profile-view-image align-items-between" style="display:none" >
+                  <button class="btn btn-primary m-2 ml-3">Save</button>
+                  <label for="pancard" class="label-preview--change btn btn-danger m-2">Change</label>
+              </div>
             </div>
           </div>
           <div class="sub-heading">Driving License</div>
           <div class="row form-group px-3 align-items-center">
             <label class="col-sm-2 p-t5 m-0" for="license">Driving License</label>
-            <div class="col-sm-6 p-0 d-flex flex-row align-items-center">
+            <div class="col-sm-10 p-0 d-flex flex-row align-items-center">
               <input
               name="license"
               id="license"
               accept=".png,.jpg,.jpeg"
               placeholder="Driving License"
               type="file"
-              onChange="img_pathUrl(this,'license-img');"
+              onChange="img_pathUrl(this,'license-img','btn-preview-section-3');"
               />
               <img src="" class="preview-img mt-2" style="display:none" id="license-img" alt="License-image">
+              <div id="btn-preview-section-3" class="profile-view-image align-items-between" style="display:none" >
+                  <button class="btn btn-primary m-2 ml-3">Save</button>
+                  <label for="license" class="label-preview--change btn btn-danger m-2">Change</label>
+              </div>
             </div>
           </div>
           

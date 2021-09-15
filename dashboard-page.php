805e56550af1468917401454edb91ccbc38fbd7a
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-15 14:51:09 +0530

    refer and earn

--- dashboard-page.php
+++ dashboard-page.php
@@ -1307,15 +1307,30 @@
         </div>
       </div>
     </div>
-    <div class="refer-main flex-column h-100" style="display:none" id="main-right-2">
+    <div class="refer-main flex-column h-100" id="main-right-2">
       <div class="heading">REFER AND EARN</div>
       <div class="card-body">
         <div class="card">
           <div class="sub-heading">Refer & Earn</div>
           <div class="referral-div d-flex flex-column align-items-center justify-content-center">
-            <img class="referral-img" src="images/referral.jpg" alt="Refer and Earn">
+            <!-- <img class="referral-img" src="images/referral.jpg" alt="Refer and Earn">
             <div class="referral-heading">Invite Friends and get Cashback upto ₹99</div>
-            <div class="referral-btn btn btn-dark">Coming Soon</div>
+            <div class="referral-btn btn btn-dark">Coming Soon</div> -->
+            <div class="referral-first">
+              <div class="referral-left text-center m-2">
+                <div class="referral-heading">Invite Friends to get Cashback upto <span class="refer-money"> ₹99</span></div>
+                <!-- <div class="referral-btn btn btn-dark">Coming Soon</div> -->
+                <hr class="w-100">
+                <div class="referral-email input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Enter Email Address" aria-label="Enter Email Address" aria-describedby="button-addon2">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="button" id="button-addon2">Invite</button>
+                  </div>
+                </div>
+              </div>
+              <img class="refer-image" src="images/refer-and-earn.png" alt="Refer and earn">
+            </div>
+            <hr class="w-100 mx-2">
           </div>
         </div>
       </div>
@@ -1477,7 +1492,7 @@
         </div>
       </div>
     </div>
-    <div class="verify-main flex-column h-100"  id="main-right-4">
+    <div class="verify-main flex-column h-100" style="display:none" id="main-right-4">
       <div class="heading">PROFILE VERIFICATION</div>
       <div class="card-body">
         <div class="card">
